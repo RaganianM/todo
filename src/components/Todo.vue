2455348370091527fb6c15fc4636ce4ed3c0013b
Margarita Raganian <m.raganyan@okenit.ru>
2020-08-06 17:24:00 +0300

    Функциональность и стили

--- src/components/Todo.vue
+++ src/components/Todo.vue
@@ -1,5 +1,34 @@
 <template>
-	<div>
-		Hello
-	</div>
+	<v-app>
+		<v-main class="d-flex flex-column align-center grey lighten-4">
+			<div class="d-flex">
+				<span class="header text-h1 mx-auto font-weight-regular my-4">todos</span>
+			</div>
+			<TodoList />
+			<div class="footer d-flex">
+				<span class="caption mx-auto mt-14 grey--text">Double-click to edit a todo</span>
+			</div>
+		</v-main>
+	</v-app>
 </template>
+
+<script>
+import TodoList from './TodoList'
+
+export default {
+  data: () => ({}),
+  components: {
+    TodoList
+  }
+}
+</script>
+
+<style scoped>
+	.header {
+		color: rgba(175, 47, 47, 0.15);
+	}
+
+	.caption {
+		font-size: 0.6em
+	}
+</style>
---
+++ src/components/TodoList.vue
@@ -0,0 +1,229 @@
+/* eslint no-tabs: ["error", { allowIndentationTabs: true }] */
+<template>
+  <div>
+		<v-card width="550" tile elevation="4">
+			<v-text-field
+				dense
+				solo
+				flat
+				v-model="newTodo"
+				class="text-h5 pl-8 pt-3 pb-3"
+				hide-details="auto"
+				placeholder="What needs to be done?"
+				@keyup.enter="addTodo"
+			>
+				<v-icon
+					v-show="todoList.length"
+					slot="prepend"
+					class="ml-n6 mt-n1"
+					large
+					color="grey lighten-1"
+					@click="toggleAllTodosChecked"
+				>mdi-chevron-down
+				</v-icon>
+			</v-text-field>
+
+			<v-list v-if="todoList.length" class="todos-list py-0" id="todos-list">
+				<v-list-item-group
+					v-for="(todo, ind) in todoList"
+					:key="ind"
+					v-show="todo.isShow"
+				>
+					<v-hover v-slot:default="{ hover }">
+					<v-list-item class="py-1">
+						<div
+							@click="toggleChecked(todo)"
+							color="grey lighten-1"
+							class="check d-flex align-center justify-center mr-7 ml-n2"
+						>
+							<v-icon v-if="todo.isDone" color="green lighten-2">mdi-check</v-icon>
+						</div>
+						<v-list-item-content>
+							<!--<v-list-item-title class="text-h5" style="color: black" :class="[todo.isDone ? 'done-todo' : '']">{{ todo.name }}</v-list-item-title>-->
+							<div @dblclick="editTodo(todo)" @blur="endEdit(todo)">
+								<v-text-field
+									type="text"
+									@blur="endEdit(todo)"
+									:disabled="!todo.isEdit"
+									ref="inputTodo"
+									v-model="todo.name"
+								>{{ todo.name }}</v-text-field>
+							</div>
+						</v-list-item-content>
+						<v-list-item-icon :class="{ 'show-close-button': hover }" class="close-button my-auto" @click="removeTodo(todo, ind)">
+							<v-icon color="brown" class="text--lighten-3" style="font-size: 1.3em">mdi-close-thick</v-icon>
+						</v-list-item-icon>
+					</v-list-item>
+					</v-hover>
+					<v-divider
+            v-if="ind + 1 < todoList.length"
+            :key="ind"
+          ></v-divider>
+				</v-list-item-group>
+			</v-list>
+			<div v-if="todoList.length" class="footer d-flex justify-space-between align-center py-1 px-3">
+				<div>
+					<span v-if="itemsLeftCount > 1" class="grey--text text--darken-2 text-body-2">{{ itemsLeftCount }} items left</span>
+					<span v-else class="grey--text text--darken-2 text-body-2">{{ itemsLeftCount }} item left</span>
+				</div>
+				<div class="pa-1">
+					<a
+						@click="showAll"
+						:class="[isShowAll ? 'active' : '']"
+						class="sort-button rounded grey--text text--darken-2 text-body-2 mx-3 py-1 px-2"
+					>All</a>
+					<a
+						@click="showActive"
+						:class="[isShowActive ? 'active' : '']"
+						class="sort-button rounded grey--text text--darken-2 text-body-2 mx-3 py-1 px-2"
+					>Active</a>
+					<a
+						@click="showCompleted"
+						:class="[isShowCompleted ? 'active' : '']"
+						class="sort-button rounded grey--text text--darken-2 text-body-2 mx-3 py-1 px-2"
+					>Completed</a>
+				</div>
+				<div class="d-flex align-center">
+					<a @click="clearCompleted" class="clear-button grey--text text--darken-2 text-body-2">Clear completed</a>
+				</div>
+			</div>
+		</v-card>
+	</div>
+</template>
+
+<script>
+export default {
+  data: () => ({
+    newTodo: '',
+    todoList: [],
+    allTodosChecked: false,
+    isShowAll: false,
+    isShowActive: false,
+    isShowCompleted: false,
+    disableInput: true
+  }),
+  computed: {
+    itemsLeftCount: function () {
+      let itemsLeft = 0
+      this.todoList.forEach(el => {
+        if (!el.isDone) itemsLeft += 1
+      })
+      return itemsLeft
+    }
+  },
+  methods: {
+    addTodo () {
+      this.todoList.push({
+        name: this.newTodo,
+        isDone: false,
+        isShow: true,
+        isEdit: false
+      })
+      this.newTodo = ''
+    },
+    removeTodo (todo) {
+      this.todoList = this.todoList.filter(item => item !== todo)
+    },
+    toggleChecked (todo, ind) {
+      todo.isDone = !todo.isDone
+    },
+    toggleAllTodosChecked () {
+      const isSomeTodoChecked = this.todoList.some(el => el.isDone)
+
+      switch (isSomeTodoChecked) {
+        case (isSomeTodoChecked && !this.allTodosChecked):
+          this.todoList.forEach(el => (el.isDone = true))
+          this.allTodosChecked = true
+          break
+        case (this.allTodosChecked): {
+          this.todoList.forEach(el => (el.isDone = false))
+          this.allTodosChecked = false
+          break
+        }
+      }
+    },
+    clearCompleted () {
+      this.todoList = this.todoList.filter(el => !el.isDone)
+    },
+    showActive () {
+      this.isShowActive = true
+      this.isShowCompleted = false
+      this.isShowAll = false
+      this.todoList.forEach(el => {
+        if (el.isDone) return (el.isShow = false)
+        el.isShow = true
+      })
+    },
+    showCompleted () {
+      this.isShowActive = false
+      this.isShowCompleted = true
+      this.isShowAll = false
+      this.todoList.forEach(el => {
+        if (el.isDone) return (el.isShow = true)
+        el.isShow = false
+      })
+    },
+    showAll () {
+      this.isShowActive = false
+      this.isShowCompleted = false
+      this.isShowAll = true
+      this.todoList.forEach(el => (el.isShow = true))
+    },
+    editTodo (todo) {
+      todo.isEdit = true
+      console.log(this.$refs.inputTodo)
+      this.$refs.inputTodo[0].focus()
+    },
+    endEdit (todo) {
+      todo.isEdit = false
+      // this.$refs.inputTodo.focus()
+    }
+  }
+}
+</script>
+
+<style scoped>
+	#todos-list {
+		border-top: 1px rgba(0, 0, 0, 0.12) solid
+	}
+
+	.footer {
+		border-top: 1px rgba(0, 0, 0, 0.12) solid
+	}
+
+	.clear-button:hover {
+		text-decoration: underline;
+	}
+
+	.sort-button:hover {
+		border: 1px rgba(175, 47, 47, 0.1) solid;
+	}
+
+	.sort-button {
+		border: 1px transparent solid;
+	}
+
+	.active {
+		border: 1px rgba(175, 47, 47, 0.2) solid;
+	}
+
+	.close-button {
+		display: none;
+	}
+
+	.show-close-button {
+		display: inline;
+	}
+
+	.check {
+		border-radius: 50%;
+		width: 2em;
+		height: 2em;
+		border: rgb(195, 195, 195) 1px solid
+	}
+
+	.done-todo {
+		text-decoration: line-through;
+		color: rgb(195, 195, 195);
+	}
+</style>
